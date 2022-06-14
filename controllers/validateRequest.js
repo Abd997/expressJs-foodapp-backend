9fbabdb3907e037725bd343c9b3c125d7cc80001
abd <abdullahashfaq997@gmail.com>
2022-06-14 19:07:05 +0500

    add validateRequest as a separate file

---
+++ controllers/validateRequest.js
@@ -0,0 +1,10 @@
+const { validationResult } = require("express-validator");
+
+const validateRequest = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+};
+
+module.exports = validateRequest;
