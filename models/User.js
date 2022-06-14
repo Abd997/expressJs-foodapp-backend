e882874c524547343393c39be7945b2713f810f3
abd <abdullahashfaq997@gmail.com>
2022-06-14 18:13:14 +0500

    add non-required field to user model

--- models/User.js
+++ models/User.js
@@ -2,12 +2,21 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
+    // required
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
+
+    // not required
+    gender: String,
+    weight: String,
+    weightGoal: String,
+    currentActivityLevel: String,
+    dateOfBirth: String,
+    Height: String,
   },
-  { collection1: "UserCollection" }
+  { collection: "UserCollection" }
 );
 
 const UserCollection = mongoose.model("UserCollection", UserSchema);
