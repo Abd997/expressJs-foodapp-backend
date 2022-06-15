1a6ad28c628d0b9bf4bd94a43d6b5508694dda60
abd <abdullahashfaq997@gmail.com>
2022-06-15 20:41:49 +0500

    complete extradetails handler

--- controllers/handleExtraDetailsRequest.js
+++ controllers/handleExtraDetailsRequest.js
@@ -1,4 +1,5 @@
 const e = require("express");
+const UserCollection = require("../models/User");
 const validateRequest = require("./validateRequest");
 
 /**
@@ -6,9 +7,23 @@ const validateRequest = require("./validateRequest");
  * @param {e.Request} req
  * @param {e.Response} res
  */
-const handleExtraDetailsRequest = (req, res) => {
+const handleExtraDetailsRequest = async (req, res) => {
   validateRequest(req, res);
-  res.send("working");
+  const doc = await UserCollection.findOneAndUpdate(
+    { email: req.body.email },
+    {
+      gender: req.body.gender,
+      weight: req.body.weight,
+      weightGoal: req.body.weightGoal,
+      currentActivityLevel: req.body.currentActivityLevel,
+      dateOfBirth: req.body.dateOfBirth,
+      height: req.body.height,
+    }
+  );
+  if (!doc) {
+    return res.status(400).send({ error: "user not found" });
+  }
+  return res.send("user details updated successfully");
 };
 
 module.exports = handleExtraDetailsRequest;
