df11ebdd6ffa8bba0d17ade3a63368a17f319815
Berugy <weareberugy@gmail.com>
2022-09-02 16:07:50 +0500

    Update bookEventTicket.js

--- src/controllers/events/bookEventTicket.js
+++ src/controllers/events/bookEventTicket.js
@@ -10,10 +10,13 @@ const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
  * @param {e.Request} req
  */
 const validate = async (req) => {
-    const { eventId } =
+    const { eventId,paymentId } =
         req.body;
     if (!eventId) {
         throw new BadRequestError("Event Id not sent");
+    }
+    if (!paymentId) {
+        throw new BadRequestError("paymentId not sent");
     }
 };
 
@@ -27,6 +30,7 @@ module.exports = async (req, res) => {
         await validate(req);
         const {
             eventId,
+            paymentId,
             loggedInUser,
         } = req.body;
 
@@ -35,20 +39,35 @@ module.exports = async (req, res) => {
                 "Customer does not have a saved payment method"
             );
         }
+        const customer = loggedInUser.stripeCustomerId;
         const event = await EventCollection.findById({ "_id": eventId })
-        await stripe.charges.create({
-            amount: `${event.fee}`,
-            description: `${event.description}`,
-            currency: "eur",
-            customer: loggedInUser.stripeCustomerId
-        }).then(async (charges) => {
-            event.participants.push(loggedInUser._id);
-            await event.save()
-            res.json({ status: "Payment Success!!!", event: event })
-        })
-            .catch(async (err) => {
-                return sendErrorResponse(res, 500, err.message);
-            })
+        const paymentIntent = await stripe.paymentIntents.create({
+			payment_method: paymentId,
+			customer: customer,
+			amount: `${event.fee}`,
+			currency: "eur",
+			confirm: "true",
+			payment_method_types: ["card"],
+			metadata: { event_id: event._id }
+		})
+		if (paymentIntent.status === "succeeded") {
+			event.participants.push(loggedInUser._id);
+            await event.save();
+		}
+		return res.json({ status: paymentIntent.status,event });
+        // await stripe.charges.create({
+        //     amount: `${event.fee}`,
+        //     description: `${event.description}`,
+        //     currency: "eur",
+        //     customer: loggedInUser.stripeCustomerId
+        // }).then(async (charges) => {
+        //     event.participants.push(loggedInUser._id);
+        //     await event.save()
+        //     res.json({ status: "Payment Success!!!", event: event })
+        // })
+        //     .catch(async (err) => {
+        //         return sendErrorResponse(res, 500, err.message);
+        //     })
 
     } catch (error) {
         return sendErrorResponse(res, 500, error.message);
